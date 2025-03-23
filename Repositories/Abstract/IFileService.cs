51e04cd5b84acf370ceb154720edd150f4547b39
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-03-23 10:58:28 +0700

    commit

---
+++ Repositories/Abstract/IFileService.cs
@@ -0,0 +1,8 @@
+namespace AspnetCoreCRUDApp.Repositories.Abstract
+{
+    public interface IFileService
+    {
+        public Tuple<int, string> SaveImage(IFormFile imageFile);
+        public bool DeleteImage(string imageFileName);
+    }
+}
